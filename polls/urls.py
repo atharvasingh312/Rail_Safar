29a3572385d0cba37e2f8c9cf7f9c99f6f5a2f6f
atharvasingh312 <111992277+atharvasingh312@users.noreply.github.com>
2024-07-21 02:51:27 +0530

    Update urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
-from . import views
 
+from . import views
+# from django.conf.urls import url
+from django.urls import path
 urlpatterns = [
     path("", views.index, name="index"),
     path('user/', views.user, name='user'),
     path('admin/', views.admin, name='admin'),
-    path('speech-to-text/',views.speech_to_text,name='speech_to_text')
+    path('speech-to-text/',views.speech_to_text,name='speech_to_text'),
+    path('record_and_transcribe/', views.record_and_transcribe, name='record_and_transcribe'),
+    path('speak_text/', views.speak_text, name='speak_text'),
 ]
