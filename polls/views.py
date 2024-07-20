32514bef0927e2ee38997006638fa95ae6bf4208
atharvasingh312 <111992277+atharvasingh312@users.noreply.github.com>
2024-07-21 02:51:02 +0530

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,48 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 import speech_recognition as sr
+from .rec_to_tran import record_audio, transcribe_speech
+from .speaking import speak_tex
+# from django.http import HttpResponse
+# from django.views.decorators.csrf import csrf_exempt
+from gtts import gTTS
+import os
+import pygame
 
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\ABHISHEK SINGH\Desktop\SIH\Rail_Safar\arctic-window-399718-fa60c692e358.json"
+
+@csrf_exempt
+def record_and_transcribe(request):
+    output_filename = r'C:\Users\ABHISHEK SINGH\Desktop\SIH\Rail_Safar\polls\op.wav'
+    record_duration = 5
+
+    # Record audio from the microphone
+    record_audio(output_filename, duration=record_duration)
+    
+    # Get the selected language from the POST data
+    target_language = request.POST.get('target')
+    # print(target_language)
+    # Transcribe the recorded audio to the selected language
+    transcribe_text = transcribe_speech(output_filename,target_language)
+
+    response_data = {
+        'transcribed_text': transcribe_text,
+    }
+
+    return JsonResponse(response_data)
+
+@csrf_exempt
+def speak_text(request):
+    if request.method == 'POST':
+        target_language = request.POST.get('target')
+        text = request.POST.get('text_data')
+        speak_tex(text, target_language)  # Call speak_text with both text and target_language
+        return HttpResponse("Text spoken.")
+    else:
+        return HttpResponse("Invalid Request")
 
 def index(request):
     return render(request,'index.html')
@@ -26,5 +66,3 @@ def speech_to_text(request):
             return JsonResponse({'error': str(e)})
     else:
         return JsonResponse({'error': 'Invalid request method'})
-
-
